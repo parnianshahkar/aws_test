f96cbdbd752fe4739e615f0b434e30187b05b5ee
pshahkar <79081595+pshahkar@users.noreply.github.com>
2022-04-06 11:47:44 +0200

    gv

--- aws_main.py
+++ aws_main.py
@@ -10,10 +10,12 @@ import pandas as pd
 # df1 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/covid-containing-subpages_aws.csv')
 # df1 = pd.read_csv('https://www.dropbox.com/s/eaxj3paf2wabeeo/covid-containing-subpages_aws.csv?dl=1')
 df1 = pd.read_csv('test.csv')
+# df2 = pd.read_csv('other_test.csv')
+# df = pd.concat([df1, df2])
+# df = df.drop_duplicates(subset=['A', 'C'], keep=False)
 
 # df2 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/top10subpages_aws.csv')
 # df2 = pd.read_csv('https://www.dropbox.com/s/5y205ipwntazbsi/top10subpages_aws.csv?dl=1')[:100]
 
 
-# df = pd.concat([df1, df2])
-print('HELLOO', df1.head())
+print('HELLOO', df1.columns)
