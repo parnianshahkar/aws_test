a6724a07ed92d1e20a2fbc882c6b2724d6fb10ab
pshahkar <79081595+pshahkar@users.noreply.github.com>
2022-04-06 11:10:09 +0200

    m

--- aws_main.py
+++ aws_main.py
@@ -8,7 +8,9 @@ import pandas as pd
 # df2 = pd.read_csv(urlrq.urlopen('https://www.dropbox.com/s/5y205ipwntazbsi/top10subpages_aws.csv?dl=1', context=ssl.create_default_context(cafile=certifi.where())))[:100]
 
 # df1 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/covid-containing-subpages_aws.csv')
-df1 = pd.read_csv('https://www.dropbox.com/s/eaxj3paf2wabeeo/covid-containing-subpages_aws.csv?dl=1')
+# df1 = pd.read_csv('https://www.dropbox.com/s/eaxj3paf2wabeeo/covid-containing-subpages_aws.csv?dl=1')
+df1 = pd.read_csv('covid-containing-subpages_aws.csv?dl=0')
+
 # df2 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/top10subpages_aws.csv')
 # df2 = pd.read_csv('https://www.dropbox.com/s/5y205ipwntazbsi/top10subpages_aws.csv?dl=1')[:100]
 
