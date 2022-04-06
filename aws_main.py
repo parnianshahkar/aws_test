e00fe87b57c31fc28a094f267a30ca69665aba9c
pshahkar <79081595+pshahkar@users.noreply.github.com>
2022-04-06 11:31:20 +0200

    l

--- aws_main.py
+++ aws_main.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # df1 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/covid-containing-subpages_aws.csv')
 # df1 = pd.read_csv('https://www.dropbox.com/s/eaxj3paf2wabeeo/covid-containing-subpages_aws.csv?dl=1')
-df1 = pd.read_csv('covid-containing-subpages_aws.csv?dl=0')
+df1 = pd.read_csv('test.csv')
 
 # df2 = pd.read_csv('/Users/parnianshahkar/Documents/KOF/Task 15/top10subpages_aws.csv')
 # df2 = pd.read_csv('https://www.dropbox.com/s/5y205ipwntazbsi/top10subpages_aws.csv?dl=1')[:100]
