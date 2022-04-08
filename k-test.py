01187d84d8647528241b6cf5d61fbb56a30c6cdb
pshahkar <79081595+pshahkar@users.noreply.github.com>
2022-04-08 11:05:58 +0200

    l

--- k-test.py
+++ k-test.py
@@ -5,13 +5,14 @@ import boto3
 import multiprocessing as mp # can use this if you need to parallelise
 from warcio.archiveiterator import ArchiveIterator
 from selectolax.parser import HTMLParser
-# import s3
 
 df = pd.read_csv('test_concatenated.csv')
 
 
 # df should have these columns: ['warc_filename, warc_record_offset, warc_record_end']
-
+s3 = boto3.client('s3', 'us-east-1',
+                aws_access_key_id=  'AKIAWGCH75G4N4ZK57SE',
+                aws_secret_access_key= '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')
 
 list_of_responses = df.apply(lambda row: s3.get_object(Bucket='commoncrawl', Key=row.warc_filename, Range='bytes=%s-%s' % (row.warc_record_offset, row.warc_record_end)))
 print(list_of_responses[0])
