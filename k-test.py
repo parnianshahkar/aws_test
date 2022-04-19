13785e33d644e42e5811a54d1dff22cf922c2f46
pshahkar <79081595+pshahkar@users.noreply.github.com>
2022-04-19 16:44:38 +0200

    b

--- k-test.py
+++ k-test.py
@@ -60,7 +60,7 @@ def func(row):
     string_format = '\n'.join(out_lst)
     paragraphs = string_format.split("\n")
     nonempty_paragraphs = [paragraph for paragraph in paragraphs if len(paragraph) > 20]
-    covid_paragraphs = [paragraph for paragraph in nonempty_paragraphs if paragraph.contains("covid")]
+    covid_paragraphs = [paragraph for paragraph in nonempty_paragraphs if "covid" in paragraph]
 
     return covid_paragraphs
 
